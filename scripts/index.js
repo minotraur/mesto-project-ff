c187b10c016e1ee53ee7bd40b618a6e08ee34b5d
Vlad Fedotov <fedotov85050@gmail.com>
2023-11-23 17:45:10 +0300

    Реализовано создание карточки и удаление

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,33 @@
-// @todo: Темплейт карточки
+// Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
 
-// @todo: DOM узлы
+// DOM узлы
+const content = document.querySelector(".content");
+const places = content.querySelector(".places__list");
+const addButton = content.querySelector(".profile__add-button");
 
-// @todo: Функция создания карточки
+// Функция создания карточки
+function createCardElement(cardTitle, cardImage) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardDeleteButton = cardElement.querySelector(".card__delete-button");
 
-// @todo: Функция удаления карточки
+  cardElement.querySelector(".card__image").src = cardImage;
+  cardElement.querySelector(".card__title").textContent = cardTitle;
 
-// @todo: Вывести карточки на страницу
+  cardDeleteButton.addEventListener("click", deleteCard);
+
+  places.append(cardElement);
+}
+
+// Функция удаления карточки
+function deleteCard(event) {
+  const cardToRemove = event.target.closest(".card");
+  if (cardToRemove) {
+    cardToRemove.remove();
+  }
+}
+
+// Вывести карточки на страницу
+initialCards.forEach((cardInfo) => {
+  createCardElement(cardInfo.name, cardInfo.link);
+});
